af41f8254793db566a60780a47df380081e6178f
Wundero <sublimecraftftw@gmail.com>
2016-03-05 21:29:49 -0500

    Added liscence to all (my) classes

--- ProjectRay/src/me/Wundero/ProjectRay/framework/CacheableSection.java
+++ ProjectRay/src/me/Wundero/ProjectRay/framework/CacheableSection.java
@@ -10,9 +10,32 @@ import org.bukkit.configuration.ConfigurationSection;
 
 import com.google.common.collect.Maps;
 
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2016 Wundero
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 public class CacheableSection extends Section implements Cacheable {
 
-	// expirable data cache (10 mins)
+	// expirable data cache (10 mins) TODO configurable expiry
 	private DataHolder cache = new DataHolder() {
 
 		private HashMap<String, Long> expiries = Maps.newHashMap();
--- ProjectRay/src/me/Wundero/ProjectRay/framework/event/JsonEvent.java
+++ ProjectRay/src/me/Wundero/ProjectRay/framework/event/JsonEvent.java
@@ -10,6 +10,29 @@ import org.bukkit.event.HandlerList;
 
 import com.google.common.collect.Lists;
 
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2016 Wundero
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 public abstract class JsonEvent extends Event implements Cancellable {
 
 	private HandlerList handlers;
@@ -26,16 +49,16 @@ public abstract class JsonEvent extends Event implements Cancellable {
 	}
 
 	public List<Sendable> getObjects() {
-		if(objects.isEmpty()) {
+		if (objects.isEmpty()) {
 			createObjects();
 		}
 		return objects;
 	}
-	
+
 	public abstract void createObjects();
-	
+
 	public void sendObjects() {
-		for(Sendable s : getObjects()) {
+		for (Sendable s : getObjects()) {
 			s.send();
 		}
 	}
